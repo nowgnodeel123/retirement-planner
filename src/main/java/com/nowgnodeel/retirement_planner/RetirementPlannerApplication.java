e55604346ebd52a36cd6667a794ca03aaae734bf
nowgnodeel123 <nowgnodeel123@gmail.com>
2026-06-28 22:21:12 +0900

    init: Spring Boot 4.1.0 프로젝트 초기 설정

---
+++ src/main/java/com/nowgnodeel/retirement_planner/RetirementPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.nowgnodeel.retirement_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetirementPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetirementPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nowgnodeel/retirement_planner/RetirementPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowgnodeel.retirement_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetirementPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
